75f25be80de5fe89693f0ca7ecfd6415fe07e719
Tennen <tianyu.chen@thoughtworks.com>
2024-07-30 09:47:30 +0800

    add constructor parameters and remove println

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnUtil.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/ColumnUtil.kt
@@ -3,10 +3,9 @@ package com.thoughtworks.kotlin_basic.util
 import com.thoughtworks.kotlin_basic.exceptions.MaxSizeExceededException
 import kotlin.math.pow
 
-class ColumnUtil {
-    private val alphabets = ('A'..'Z').toList()
+class ColumnUtil(private val alphabets: List<Char> = ('A'..'Z').toList(), times: Int = 3) {
     private val digits = alphabets.size
-    private val maxSize = digits.toDouble().pow(3).toInt()
+    private val maxSize = digits.toDouble().pow(times).toInt()
 
     fun convertToAlphabeticalLabels(start: Int, numbers: Int): Array<String> {
         val end = start + numbers - 1
@@ -15,10 +14,8 @@ class ColumnUtil {
         for (i in start .. end) {
             val alphabetList = mutableListOf<String>()
             var number = i
-            println(i)
             while (number > 0) {
                 val alphabetIndex = (number - 1) % digits
-                println(alphabetIndex)
                 alphabetList.add(alphabets[alphabetIndex].toString())
                 /* 26不进位 */
                 if (number == digits) break
